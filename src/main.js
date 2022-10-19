9b121bd18dc7ccec7dcce01c71dfc9ac373ff12c
BrunoGurris <brunoagpires@gmail.com>
2022-10-19 01:10:51 -0300

    feat(*): add toast

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueToastr from "vue-toastr";
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -11,6 +12,7 @@ import vuetify from './plugins/vuetify'
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueToastr)
 
 new Vue({
     router,
